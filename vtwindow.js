a248ba72d2cf8033d04c096e5b4ac61dc9c9888a
victornpb <3372598+victornpb@users.noreply.github.com>
2018-09-11 23:27:50 -0300

    remove deadcode

--- vtwindow.js
+++ vtwindow.js
@@ -1,5 +1,3 @@
-function noop() { }
-
 /**
  * A Virtual windows system for the browser written in vanilla js
  * @see https://github.com/victornpb/VtWindow
@@ -660,34 +658,34 @@ class Drag {
 
 // export default VtWindow;
 
-function throttle(func, wait, options) {
-  var context, args, result;
-  var timeout = null;
-  var previous = 0;
-  if (!options) options = {};
-  var later = function() {
-    previous = options.leading === false ? 0 : Date.now();
-    timeout = null;
-    result = func.apply(context, args);
-    if (!timeout) context = args = null;
-  };
-  return function() {
-    var now = Date.now();
-    if (!previous && options.leading === false) previous = now;
-    var remaining = wait - (now - previous);
-    context = this;
-    args = arguments;
-    if (remaining <= 0 || remaining > wait) {
-      if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
-      }
-      previous = now;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining);
-    }
-    return result;
-  };
-}
+// function throttle(func, wait, options) {
+//   var context, args, result;
+//   var timeout = null;
+//   var previous = 0;
+//   if (!options) options = {};
+//   var later = function() {
+//     previous = options.leading === false ? 0 : Date.now();
+//     timeout = null;
+//     result = func.apply(context, args);
+//     if (!timeout) context = args = null;
+//   };
+//   return function() {
+//     var now = Date.now();
+//     if (!previous && options.leading === false) previous = now;
+//     var remaining = wait - (now - previous);
+//     context = this;
+//     args = arguments;
+//     if (remaining <= 0 || remaining > wait) {
+//       if (timeout) {
+//         clearTimeout(timeout);
+//         timeout = null;
+//       }
+//       previous = now;
+//       result = func.apply(context, args);
+//       if (!timeout) context = args = null;
+//     } else if (!timeout && options.trailing !== false) {
+//       timeout = setTimeout(later, remaining);
+//     }
+//     return result;
+//   };
+// }
